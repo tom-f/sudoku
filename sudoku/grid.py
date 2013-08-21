dd23e4adbb731af6c45ac1a3019e11fd00e729de
Tom Fawssett <tom.fawssett@gmail.com>
2013-08-21 18:21:28 +0100

    Ensure grid items are loaded as integers

--- sudoku/grid.py
+++ sudoku/grid.py
@@ -4,13 +4,21 @@ class Grid:
 	def __init__(self):
 		self.grid = [None] * 81
 
+	def load_data(self, data_string, separator=","):
+		"""Take string with values split by given seperator."""
+		self.grid = [int(x) for x in data_string.split(separator)]
+
 	def print_grid(self):
 		for row in range(9):
 			row = self.grid[9*row:9*row+9]
 			print row
 
+	def get_grid(self):
+		return self.grid
+
 	def row(self, index):
-		pass
+		row = index/9
+		return self.grid[row*9:row*9*9]
 
 	def coloumn(self, index):
 		pass
