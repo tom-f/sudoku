1ca26b65f5695228097f5596fa13b2f0b76e1029
Tom Fawssett <tom.fawssett@gmail.com>
2013-08-30 10:39:01 +0100

    Update print_grid to use pretty ascii formatting

--- sudoku/grid.py
+++ sudoku/grid.py
@@ -1,45 +1,52 @@
+from __future__ import print_function
 
 class Grid:
 
-	def __init__(self):
-		self.grid = [None] * 81
+    def __init__(self):
+        self.grid = [None] * 81
 
-	def load_data(self, data_string, separator=","):
-		"""Take string with values split by given separator."""
-		self.grid = [int(x) for x in data_string.split(separator)]
+    def load_data(self, data_string, separator=","):
+        """Take string with values split by given separator."""
+        self.grid = [(lambda x: int(x) if x != '' else 0)(x) for x in data_string.split(separator)]
 
-	def print_grid(self):
-		for row in range(9):
-			row = self.grid[9*row:9*row+9]
-			print row
+    def print_grid(self):
+        print("+---+---+---+---+---+---+---+---+---+")
+        for row in range(1,10):
+            self.print_row(self.get_row(row))
 
-	def get_grid(self):
-		return self.grid
+    def print_row(self, row):
+        for index, elm in enumerate(row):
+            print("| ", end="")
+            print(elm, end="")
+            print(" ", end="")
+            if index == len(row)-1:
+                print("|")
+        print("+---+---+---+---+---+---+---+---+---+")
 
-	def get_row(self, row_index):
-		"""Return requested row (top down from 1)"""
+    def get_grid(self):
+        return self.grid
 
-		# Because list indexing starts from 0
-		row_index = row_index-1
-		return self.grid[row_index*9:row_index*9+9]
+    def get_row(self, row_index):
+        """Return requested row (top down from 1)"""
+        # Because list indexing starts from 0
+        row_index = row_index-1
+        return self.grid[row_index*9:row_index*9+9]
 
-	def get_column(self, column_index):
-		"""Return requested column (left to right from 1)"""
-		
-		# Because list indexing starts from 0
-		column_index = column_index-1
-		return self.grid[column_index::9]
+    def get_column(self, column_index):
+        """Return requested column (left to right from 1)"""
+        # Because list indexing starts from 0
+        column_index = column_index-1
+        return self.grid[column_index::9]
 
-	def get_square(self, square_index):
-		"""Return requested sub square (from top left by row)"""
+    def get_square(self, square_index):
+        """Return requested sub square (from top left by row)"""
+        # Initial square position (top left) using tn = dn+(a-d)
+        # with one extra because list index from 0
+        initial = (((3*square_index)-2)-1)
 
-		# Initial square position (top left) using tn = dn+(a-d) 
-		# with one extra because list index from 0
-		initial = (((3*square_index)-2)-1)
+        square = []
 
-		square = []
-
-		square.extend(self.grid[initial:initial+3])
-		square.extend(self.grid[initial+9:initial+12])
-		square.extend(self.grid[initial+18:initial+21])
-		return square
+        square.extend(self.grid[initial:initial+3])
+        square.extend(self.grid[initial+9:initial+12])
+        square.extend(self.grid[initial+18:initial+21])
+        return square
--- sudoku/main.py
+++ sudoku/main.py
@@ -4,13 +4,7 @@ class Main:
 
 	def run(self):
 		"""Main access point for application"""
-		grid = Grid()
-		expected_string = ','.join((str(x) for x in range(1,82)))
-		grid.load_data(expected_string)
-		grid.print_grid()
-
-		print "\n"
-		print "\n"
-
-		col = grid.get_column(2)
-		print col
+        grid = Grid()
+        puzzle_string = ",6,,2,9,4,,7,,2,,8,,,,9,,4,,9,,,5,,,2,,7,,,5,,3,,,2,9,,1,,,,5,,7,5,,,1,,9,,,3,,1,,,4,,,3,,3,,2,,,,7,,9,,4,,9,3,2,,1,"
+        grid.load_data(puzzle_string)
+        grid.print_grid()
