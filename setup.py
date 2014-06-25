e32ecfe4ec01c1cf288e76b70dbf51cdcd01047f
Tom Fawssett <tom.fawssett@gmail.com>
2014-06-25 10:49:32 +0100

    Move test script into installable script

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ config = {
     'version': '0.1',
     'install_requires': ['nose'],
     'packages': ['sudoku'],
-    'scripts': [],
+    'scripts': ['bin/sudoku-test'],
     'name': 'sudoku'
 }
 
