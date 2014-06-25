b7de89c9a6fd2f2a57dda9bdc4c10306396828a0
Tom Fawssett <tom.fawssett@gmail.com>
2014-06-25 12:02:59 +0100

    Update main to draw a given grid from string

--- sudoku/main.py
+++ sudoku/main.py
@@ -3,7 +3,8 @@ from grid import Grid
 class Main:
 
 	def run(self):
-		print "bye"
-
-		grid = Grid()
-		grid.print_grid()
+		"""Main access point for application"""
+        grid = Grid()
+        puzzle_string = ",6,,2,9,4,,7,,2,,8,,,,9,,4,,9,,,5,,,2,,7,,,5,,3,,,2,9,,1,,,,5,,7,5,,,1,,9,,,3,,1,,,4,,,3,,3,,2,,,,7,,9,,4,,9,3,2,,1,"
+        grid.load_data(puzzle_string)
+        grid.print_grid()
